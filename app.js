46c73423861ff7241104a7572c777840ab8c166e
alexandre-mace <contact.alexandremace@gmail.com>
2020-10-27 03:00:31 +0100

    Update UI

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 window.onload = function() {
-    let mediaQuery = window.matchMedia("(max-width: 970px)");
+    // let mediaQuery = window.matchMedia("(max-width: 970px)");
 
     // CURRENT PROJECT NOTIFIER
-    const projects = document.getElementsByClassName('project');
-    if (!mediaQuery.matches) {
-
-        Array.from(projects).forEach(function (project) {
-                project.addEventListener("mouseover", function (event) {
-                    if (this.classList.contains('project') && !event.target.classList.contains('project-label')) {
-
-                        // display the displayed project info
-                        document.getElementById('displayed-project-description').replaceChild(this.querySelector('.project-details').cloneNode(true), document.getElementById('displayed-project-description').children[0]);
-
-                        // remove all existing displayed project notifier and all active labels
-                        Array.from(document.getElementsByClassName('displayed-project-notifier')).forEach(function (displayedProjectNotifier) {
-                            displayedProjectNotifier.remove();
-                        });
-                        Array.from(document.getElementsByClassName('project-label')).forEach(function (label) {
-                            label.classList.remove('project-label-active');
-                        });
-                        // display the displayed project notifier
-                        this.getElementsByClassName('project-label')[0].classList.add('project-label-active');
-
-                        const displayedProjectNotifier = document.createElement('span');
-                        displayedProjectNotifier.innerHTML = '';
-                        displayedProjectNotifier.classList.add('displayed-project-notifier');
-                        this.getElementsByClassName('project-title')[0].prepend(displayedProjectNotifier);
-                    }
-                });
-        })
-
-        // DISPLAY 1ST PROJECT BY DEFAULT
-        const firstProject = document.getElementsByClassName('project')[0];
-        const mouseoverEvent = new Event('mouseover');
-        firstProject.dispatchEvent(mouseoverEvent);
-    }
+    // const projects = document.getElementsByClassName('project');
+    // if (!mediaQuery.matches) {
+    //
+    //     Array.from(projects).forEach(function (project) {
+    //             project.addEventListener("mouseover", function (event) {
+    //                 if (this.classList.contains('project') && !event.target.classList.contains('project-label')) {
+    //
+    //                     // display the displayed project info
+    //                     document.getElementById('displayed-project-description').replaceChild(this.querySelector('.project-details').cloneNode(true), document.getElementById('displayed-project-description').children[0]);
+    //
+    //                     // remove all existing displayed project notifier and all active labels
+    //                     Array.from(document.getElementsByClassName('displayed-project-notifier')).forEach(function (displayedProjectNotifier) {
+    //                         displayedProjectNotifier.remove();
+    //                     });
+    //                     Array.from(document.getElementsByClassName('project-label')).forEach(function (label) {
+    //                         label.classList.remove('project-label-active');
+    //                     });
+    //                     // display the displayed project notifier
+    //                     this.getElementsByClassName('project-label')[0].classList.add('project-label-active');
+    //
+    //                     const displayedProjectNotifier = document.createElement('span');
+    //                     displayedProjectNotifier.innerHTML = '';
+    //                     displayedProjectNotifier.classList.add('displayed-project-notifier');
+    //                     this.getElementsByClassName('project-title')[0].prepend(displayedProjectNotifier);
+    //                 }
+    //             });
+    //     })
+    //
+    //     // DISPLAY 1ST PROJECT BY DEFAULT
+    //     const firstProject = document.getElementsByClassName('project')[0];
+    //     const mouseoverEvent = new Event('mouseover');
+    //     firstProject.dispatchEvent(mouseoverEvent);
+    // }
     // RANDOM SKILL DISPLAY
     // const skills = ['symfony', 'api platform', 'react', 'vuejs', 'javascript', 'mysql', 'UML', 'testing', 'travis'];
     // window.setInterval(function(){
@@ -65,25 +65,25 @@ window.onload = function() {
     };
 
     // MOBILE PROJECTS DESCRIPTIONS
-    function showMobileDescription(mediaQuery) {
-        if (mediaQuery.matches) {
-            Array.from(document.getElementsByClassName('project')).forEach(function (project) {
-                const mobileProjectDescription = project.getElementsByClassName('mobile-project-description')[0];
-                if (mobileProjectDescription.children.length > 0) {
-                    mobileProjectDescription.replaceChild(mobileProjectDescription.parentNode.querySelector('.project-details').cloneNode(true), mobileProjectDescription.children[0]);
-                }
-            });
-            Array.from(document.getElementsByClassName('displayed-project-notifier')).forEach( function (displayedProjectNotifier) {
-                displayedProjectNotifier.remove();
-            });
-        } else {
-            Array.from(document.getElementsByClassName('project')).forEach(function (project) {
-                const mobileProjectDescription = project.getElementsByClassName('mobile-project-description')[0];
-                mobileProjectDescription.innerHTML = '';
-            })
-        }
-    }
-
-    showMobileDescription(mediaQuery)
-    mediaQuery.addListener(showMobileDescription)
+    // function showMobileDescription(mediaQuery) {
+    //     if (mediaQuery.matches) {
+    //         Array.from(document.getElementsByClassName('project')).forEach(function (project) {
+    //             const mobileProjectDescription = project.getElementsByClassName('mobile-project-description')[0];
+    //             if (mobileProjectDescription.children.length > 0) {
+    //                 mobileProjectDescription.replaceChild(mobileProjectDescription.parentNode.querySelector('.project-details').cloneNode(true), mobileProjectDescription.children[0]);
+    //             }
+    //         });
+    //         Array.from(document.getElementsByClassName('displayed-project-notifier')).forEach( function (displayedProjectNotifier) {
+    //             displayedProjectNotifier.remove();
+    //         });
+    //     } else {
+    //         Array.from(document.getElementsByClassName('project')).forEach(function (project) {
+    //             const mobileProjectDescription = project.getElementsByClassName('mobile-project-description')[0];
+    //             mobileProjectDescription.innerHTML = '';
+    //         })
+    //     }
+    // }
+    //
+    // showMobileDescription(mediaQuery)
+    // mediaQuery.addListener(showMobileDescription)
 };
