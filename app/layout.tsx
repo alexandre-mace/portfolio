166dae0a8c69949dd80b0c69bd8df5f91493c389
Alexandre Macé <helloalexandremace@protonmail.com>
2025-07-20 17:15:42 +0200

    feat(og image): add screenshot

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,25 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.png',
   },
+  openGraph: {
+    title: "Alexandre Macé - Full stack developer",
+    description: "Alexandre Macé, fullstack developer working with symfony and react. Taking part in projects tied to climate or biodiversity.",
+    images: [
+      {
+        url: '/og-image-portfolio.png',
+        width: 1200,
+        height: 630,
+        alt: 'Alexandre Macé Portfolio Preview',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Alexandre Macé - Full stack developer",
+    description: "Alexandre Macé, fullstack developer working with symfony and react. Taking part in projects tied to climate or biodiversity.",
+    images: ['/og-image-portfolio.png'],
+  },
 };
 
 export default function RootLayout({
